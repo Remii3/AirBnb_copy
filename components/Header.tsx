d9057050ecfe795df4289615aafae12f7414fe82
Remigiusz Woźniak <remek778790@gmail.com>
2022-11-30 23:59:13 +0100

    info cards, calendar, loading bar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import {
   MagnifyingGlassIcon,
@@ -6,10 +6,54 @@ import {
   GlobeAltIcon,
   Bars3Icon,
 } from '@heroicons/react/24/solid';
-const Header = () => {
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
+import { useRouter } from 'next/router';
+
+const Header = ({ placeholder }: { placeholder: string }) => {
+  const [searchInput, setSearchInput] = useState('');
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [numberOfGuests, setNumberOfGuests] = useState('1');
+  const router = useRouter();
+
+  const selectionRange = {
+    startDate,
+    endDate,
+    key: 'selection',
+  };
+
+  const selectHandler = (ranges: any) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
+  const cancelHandler = () => {
+    setSearchInput('');
+    setStartDate(new Date());
+    setEndDate(new Date());
+    setNumberOfGuests('1');
+  };
+
+  const searchHandler = () => {
+    router.push({
+      pathname: '/search',
+      query: {
+        location: searchInput,
+        startDate: startDate.toISOString(),
+        endDate: endDate.toISOString(),
+        numberOfGuests: numberOfGuests,
+      },
+    });
+  };
+
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
-      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+      <div
+        onClick={() => router.push('/')}
+        className="relative flex items-center h-10 cursor-pointer my-auto"
+      >
         <Image
           src={'https://links.papareact.com/qd3'}
           alt="airbnb_image"
@@ -20,7 +64,9 @@ const Header = () => {
       <div className="flex items-center rounded-full md:border-2 py-2">
         <input
           type="text"
-          placeholder="Start your search"
+          value={searchInput}
+          onChange={e => setSearchInput(e.target.value)}
+          placeholder={placeholder ? placeholder : 'Start your search'}
           className="pl-5 bg-transparent outline-none flex-grow"
         />
         <MagnifyingGlassIcon className="h-8 bg-red-400 text-white p-2 rounded-full cursor-pointer hidden md:inline-flex md:mx-2" />
@@ -33,6 +79,43 @@ const Header = () => {
           <UserIcon className="h-6" />
         </div>
       </div>
+      {searchInput && (
+        <div className="flex flex-col col-span-3 mx-auto mt-10">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={['#fd5b61']}
+            onChange={ranges => selectHandler(ranges)}
+          />
+          <div className="flex items-center border-b mb-4">
+            <h2 className="text-2xl flex-grow font-semibold">
+              Number of guests
+            </h2>
+            <UserIcon className="h-5" />
+            <input
+              type="number"
+              value={numberOfGuests}
+              onChange={e => setNumberOfGuests(e.target.value)}
+              min={'1'}
+              className="w-12 pl-2 text-lg outline-none text-red-400"
+            />
+          </div>
+          <div className="flex">
+            <button
+              className="flex-grow text-gray-500"
+              onClick={() => cancelHandler()}
+            >
+              Cancel
+            </button>
+            <button
+              onClick={() => searchHandler()}
+              className="flex-grow text-red-400"
+            >
+              Search
+            </button>
+          </div>
+        </div>
+      )}
     </header>
   );
 };
